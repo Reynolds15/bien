6af0c62833a0138eeef71ca04bf1aa43608dd77b
Brandon <brandon-reynolds@outlook.com>
2019-10-16 11:54:20 +1100

    added ambience and SEO

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_013537) do
+ActiveRecord::Schema.define(version: 2019_10_16_003645) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_10_09_013537) do
     t.string "cuisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.string "ambience"
   end
 
 end
